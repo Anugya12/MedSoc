3b70e65bebbce111f91506f4b2e8d416fc0fc4f5
Anugya Baraik <83193763+Anugya12@users.noreply.github.com>
2022-09-11 17:05:15 +0530

    sec 3

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
+/*******************navbar******************/
 $('a').click(function(e){
     e.preventDefault();
     $('body, html').animate({
         scrollTop: $( $(this).attr('href') ).offset().top - 120
     }, 1000);
+});
+/*****************register******************/
+const inputs = document.querySelectorAll(".input");
+
+function focusFunc() {
+  let parent = this.parentNode;
+  parent.classList.add("focus");
+}
+
+function blurFunc() {
+  let parent = this.parentNode;
+  if (this.value == "") {
+    parent.classList.remove("focus");
+  }
+}
+
+inputs.forEach((input) => {
+  input.addEventListener("focus", focusFunc);
+  input.addEventListener("blur", blurFunc);
 });
